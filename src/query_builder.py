3e3f9e6bd30a7adbd894a80c61c4cd84a7843fa4
amrut <amrutjadhav2294@gmail.com>
2016-11-13 21:18:26 +0530

    error handling

--- src/query_builder.py
+++ src/query_builder.py
@@ -4,21 +4,25 @@ import re
 from query_helper import Helper
 from databasehandler import DatabaseHandler
 
-class QueryBuilder:
+class QueryBuilder(Exception):
 	
 	def __init__(self):
 		""" Initialize """
 		self.databasehandler = DatabaseHandler()
 		self.helper = Helper()
-		print("query builder class is initialized.")
+		print("Hello there..! Query about your system files..")
 
 	def builder(self,user_query):
 		""" Build db query parsing user's query """ 
 		file_tense = self.find_tense(user_query)			# Return +1/-1 
 		timestamp,timestamp_query = self.find_time_query(user_query)  # Return date & [day/month/year]
 		file_mode = self.find_file_mode(user_query)		# Return [create/modify/delete]=[0,4,9]
-		print(file_tense,timestamp,timestamp_query,file_mode)
-		self.build_query(file_tense,timestamp,timestamp_query,file_mode)
+		result = self.build_query(file_tense,timestamp,timestamp_query,file_mode)
+		if result:
+			for doc in result:
+				print(doc["name"])
+		else:
+			print("Sorry, I don't found any related files")
 
 	def build_query(self,file_tense,timestamp,timestamp_query,file_mode):
 		""" Build mongodb query """
@@ -27,16 +31,15 @@ class QueryBuilder:
 
 		if file_mode == 0:
 			# db.meta.aggregate({$project:{name:1,month:{$month:'$create'}}},{$match:{month:11}})
-			self.databasehandler.execute_query(timestamp_query,"$"+timestamp_query,"$create",timestamp)
+			return self.databasehandler.execute_query(timestamp_query,"$"+timestamp_query,"$create",timestamp)
 		elif file_mode == 4:
-			self.databasehandler.execute_query(timestamp_query,"$"+timestamp_query,"$modify",timestamp)
+			return  self.databasehandler.execute_query(timestamp_query,"$"+timestamp_query,"$modify",timestamp)
 		elif file_mode == 9:
-			self.databasehandler.execute_query(timestamp_query,"$"+timestamp_query,"$delete",timestamp)
+			return  self.databasehandler.execute_query(timestamp_query,"$"+timestamp_query,"$delete",timestamp)
 
 
 	def find_time_query(self,user_query):
 		""" Return time"""
-		print( user_query)
 		if "day" in user_query:
 			 return self.helper.get_present_day(),"dayOfMonth"
 		elif "month" in user_query:
@@ -44,7 +47,7 @@ class QueryBuilder:
 		elif "year" in user_query:
 			return  self.helper.get_present_year(),"year"
 		else:
-			print("wrong query")
+			raise Exception("Specify search date operator i.e day,month,year")
 
 	def find_tense(self,user_query):
 		""" Find the tense of user query """
@@ -54,6 +57,8 @@ class QueryBuilder:
 			return -1
 		elif any(literal in user_query for literal in present_tense_literals):
 			return  1
+		else:
+			raise Exception("Specify tense i.e previous,today etc.")
 
 	def find_file_mode(self,user_query):
 		""" Return file search mode """
@@ -63,6 +68,8 @@ class QueryBuilder:
 			return 4
 		elif "delete" in user_query:
 			return 9
+		else:
+			raise Exception("Specify file mode i.e create,modify,delete")
 
 	# def find_days(self,user_query):
 	# 	""" Find the days of user query """ 
@@ -73,11 +80,13 @@ class QueryBuilder:
 
 if __name__=='__main__':
 	query_builder = QueryBuilder()
-	try:
-		while True:
+	while True:
+		try:
 			query = raw_input("Enter your query = ")
 			query_builder.builder(query)
-	except KeyboardInterrupt:								# Stop execution if ctrl+'c' hits teminal
-		query.databasehandler.close_db_connection()
-	finally:
-		print("Deep file search engine terminated")
+		except Exception as e:								
+			print("[Error]=>"+str(e))
+		except KeyboardInterrupt:								# Stop execution if ctrl+'c' hits teminal
+			query_builder.databasehandler.close_connection()
+			print("Deep file search engine terminated")
+			break
